c784567b6bd5c5f5c8f37fc83d77e6e90fef73ba
Hrishikesh Potnis <131806487+hrishikesh3304@users.noreply.github.com>
2024-05-27 09:49:00 +0530

    Update cryptoModule.h -> POST Methods

--- cryptoModule.h
+++ cryptoModule.h
@@ -13,15 +13,20 @@ class cryptoModule {
 public:
     cryptoModule(uint8_t rxPin, uint8_t txPin); 
     void initialize();
+
     bool sendWiFiCredentials(const char* ssid, const char* pass);
+
     void setServerDetails(const char* server, const char* protocol);
     String get_serverName();
     String get_protocol();
-    void GET(String query_parameters, String query_parameters_encrypt);
-    int POST();
-    void generate_key(int length);
-    void get_key();
-    void set_key();
+
+    void GET_query(String query_parameters, String query_parameters_encrypt);
+    void POST_JSON(String JSONobj);
+    void POST_query(String POSTdata);
+    //void POST_JSON();
+    const char* generate_key(int length);
+    String get_key();
+    void set_key(const char* key);
 private:
     SoftwareSerial _espSerial;
     String _serverName;
