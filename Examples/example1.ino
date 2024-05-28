356627ae59d79d70e2bc521782f60502d77d184b
Hrishikesh Potnis <131806487+hrishikesh3304@users.noreply.github.com>
2024-05-29 01:38:12 +0530

    Update example1.ino -> Generic example

--- Examples/example1.ino
+++ Examples/example1.ino
@@ -1,19 +1,32 @@
-#include <cryptoModule.h>
+#include <CryptoWifiClient.h>
 
-cryptoModule esp8266(2, 3); 
+CryptoWifiClient esp8266(2, 3); 
+String query_parameters;
+String str;
 
-void setup()  
+void setup() 
 {
-    Serial.begin(9600);
-    esp8266.initialize();
-    esp8266.sendWiFiCredentials("OnePlus 7", "123456789");
-    esp8266.setServerDetails("http://blr1.blynk.cloud/external/api/update", HTTP);
-    String query_parameters = "v2=11";
-    String str = "token=N2WqLylm5Dlc4NzyIoQghr5Zyz-cdhDd";
-    esp8266.GET(str,query_parameters);
+  pinMode(6, OUTPUT);
+  Serial.begin(9600);     
+  Serial.println();
+  esp8266.initialize();
+  esp8266.sendWiFiCredentials("OnePlus 7", "123456789"); 
+  esp8266.setServerDetails("http://blr1.blynk.cloud/external/api/update", HTTP); 
+  esp8266.setKey(esp8266.generateKey(10));
+
+  Serial.println("\nApproximate your card to the reader...");
+  str = "token=N2WqLylm5Dlc4NzyIoQghr5Zyz-cdhDd";
 }
 
 void loop() 
 {
+  query_parameters = "v3=";
+  String uidString = getData();
+  query_parameters += uidString;
+  esp8266.getQuery(str,query_parameters);
+}
 
+String getData() 
+{
+  //Logic for reading sensor data
 }
